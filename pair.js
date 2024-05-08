60c154177e649f9d522c3c8a7b41332f1dbc17e8
EXCEL <119881740+excelottah6@users.noreply.github.com>
2024-05-08 21:04:01 +0000

    Hope it fixes

--- pair.js
+++ pair.js
@@ -12,15 +12,15 @@ const {
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true })
- };
+};
 router.get('/', async (req, res) => {
     let num = req.query.number;
-        async function XeonPair() {
+    async function XeonPair() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState(`./session`)
-     try {
+        try {
             let Wrld = makeWASocket({
                 auth: {
                     creds: state.creds,
@@ -29,15 +29,15 @@ router.get('/', async (req, res) => {
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: [ "Ubuntu", "Chrome", "20.0.04" ],
-             });
-             if(!Wrld.authState.creds.registered) {
+            });
+            if(!Wrld.authState.creds.registered) {
                 await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Wrld.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
+                num = num.replace(/[^0-9]/g,'');
+                const code = await Wrld.requestPairingCode(num)
+                if(!res.headersSent){
+                    await res.send({code});
+                }
+            }
             Wrld.ev.on('creds.update', saveCreds)
             Wrld.ev.on("connection.update", async (s) => {
                 const {
@@ -45,32 +45,32 @@ router.get('/', async (req, res) => {
                     lastDisconnect
                 } = s;
                 if (connection == "open") {
-                await delay(10000);
+                    await delay(10000);
                     const sessionizuku = fs.readFileSync('./session/creds.json');
                     const izuku = fs.readFileSync('./Wrld.mp3');
-                    Wrld.groupAcceptInvite("HxVuy25MtqoFOsYuyxBx0G");const b64Session = Buffer.from(sessionizuku).toString("base64");
-const message = "IZUKU;;;" + b64Session; 
-const black = await Wrld.sendMessage(Wrld.user.id, { text: message });
+                    Wrld.groupAcceptInvite("HxVuy25MtqoFOsYuyxBx0G");
+                    const b64Session = Buffer.from(sessionizuku).toString("base64");
+                    const message = "IZUKU;;;" + b64Session; 
+                    const black = await Wrld.sendMessage(Wrld.user.id, { text: message });
 
-Wrld.sendMessage(Wrld.user.id, {
-    audio: izuku,
-    mimetype: 'audio/mp4',
-    ptt: true
-}, {
-    quoted: black
-});
+                    Wrld.sendMessage(Wrld.user.id, {
+                        audio: izuku,
+                        mimetype: 'audio/mp4',
+                        ptt: true
+                    }, {
+                        quoted: black
+                    });
 
-
-				await Wrld.sendMessage(Wrld.user.id, { text: `
+                    await Wrld.sendMessage(Wrld.user.id, { text: `
    ┍━━☽ IZUKU-MD ☾━━┑
  ༻༺━━━━⁎∗.*.∗⁎━━━━༻༺
     🛑Do not share YOUR SESSION ID with anybody\n\n© 
     YOU CAN FOLLOW @wrld.iz on TIKTOK (cringe)
    ༻༺━━━━⁎∗.*.∗⁎━━━━༻༺ ` }, {quoted: black});
-        await delay(100);
-        return await removeFile('./session');
-        process.exit(0)
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(100);
+                    await removeFile('./session');
+                    process.exit(0);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     XeonPair();
                 }
@@ -78,24 +78,24 @@ Wrld.sendMessage(Wrld.user.id, {
         } catch (err) {
             console.log("service restated");
             await removeFile('./session');
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
+            if(!res.headersSent){
+                await res.send({code:"Service Unavailable"});
+            }
         }
     }
     return await XeonPair()
 });
 
 process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("conflict")) return
-if (e.includes("Socket connection timeout")) return
-if (e.includes("not-authorized")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
+    let e = String(err)
+    if (e.includes("conflict")) return
+    if (e.includes("Socket connection timeout")) return
+    if (e.includes("not-authorized")) return
+    if (e.includes("rate-overlimit")) return
+    if (e.includes("Connection Closed")) return
+    if (e.includes("Timed Out")) return
+    if (e.includes("Value not found")) return
+    console.log('Caught exception: ', err)
 })
 
 module.exports = router
